c7e37c3abd752037ebae95e1096c2acb03e58d5e
Mahdi-Ebp <ebpmahdi@gmail.com>
2024-01-30 19:42:52 +0330

    debugged some more

--- chapter2/insertion_sort.cpp
+++ chapter2/insertion_sort.cpp
@@ -4,11 +4,13 @@ using namespace std;
 void sort(int a[], int n){
 	int t = 0;
 	for(int i = 1; i < n; i++)
-		for(int j = i - 1; j >= 0 && a[j + 1] < a[j]; j--){
-			t = a[j + 1];
+	{
+		t = a[i];
+		int j;
+		for(j = i - 1; j >= 0 && t < a[j]; j--)
 			a[j + 1] = a[j];
-			a[j] = t;
-		}
+		a[j + 1] = t;
+	}
 }
 
 int main(){
